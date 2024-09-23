8b0f8578b1fc23b389f76166cc4d126a31ba03ec
nycck <vanbeusichemnick@gmail.com>
2024-09-23 11:22:25 +0200

    Einde video 7

--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,22 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50.000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10.000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40.000',
-            ],
-        ]
-    ]);
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50.000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10.000',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40.000',
-        ],
-    ];
+    $job = Job::find($id);
 
-    $job = \Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
-    
     return view('job', ['job' => $job]);
 });
 
-
 Route::get('/contact', function () {
-
     return view('contact');
 });
